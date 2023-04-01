3bafd9358b9bda62dbdaa9d4734b51d321c11b9c
xiaobaozi12 <128770285+xiaobaozi12@users.noreply.github.com>
2023-04-01 09:49:53 +0800

    Update 3_29.cpp

--- 3_29.cpp
+++ 3_29.cpp
@@ -291,3 +291,31 @@ int main(){
 //	Fra(n, 'a', 'b', 'c');
 //	return 0;
 //}
+#include<stdio.h>
+void bubble_sort(int arr[],int sz) {
+	//int sz = sizeof(arr) / sizeof(arr[0]);
+	int i, j;
+	for (i = 0; i <sz - 1; i++) {
+		for (j = 0; j <sz - 1 - i; j++) {
+			
+			if (arr[j] < arr[j + 1]) {
+				int tmp = 0;
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+
+			}
+		}
+	}
+
+}
+int main() {
+	int arr[] = { 0,1,2,3,4,5,6,7,8,9,10, };
+	
+	int i = 0;
+	int sz = sizeof(arr)/sizeof(arr[0]);
+	bubble_sort(arr,sz);
+	for (i = 0; i < sz; i++) {
+		printf("%d", arr[i]);
+	}
+}
