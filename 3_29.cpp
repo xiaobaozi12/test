2afd87524a21ef70867ff9412174db929f524005
xiaobaozi12 <128770285+xiaobaozi12@users.noreply.github.com>
2023-05-07 17:10:52 +0800

    Update 3_29.cpp

--- 3_29.cpp
+++ 3_29.cpp
@@ -1,5 +1,118 @@
 #include<stdio.h>
 #include<stdlib.h>
+//#include<stdio.h>
+//#include<stdlib.h>
+////void test() {
+////	printf("***************************\n");
+////	printf("1.add   2.sub  3.mul  4.div\n");
+////	printf("            0.break        \n");
+////	printf("****************************\n");
+////
+////}
+////int add(int x, int y) {
+////	return x + y;
+////}int sub(int x, int y) {
+////	return x - y;
+////}int mul(int x, int y) {
+////	return x * y;
+////}int div(int x, int y) {
+////	return x / y;
+////}
+////int main() {
+////	int input = 0;
+////	int x, y;
+////	int(*arr[])(int, int) = { 0,add,sub,mul,div };
+////	do {
+////		test();
+////		scanf("%d", &input);
+////		if (input == 0) {
+////			break;
+////		}
+////		else if (input <= 5 && input > 0) {
+////			printf(" 请输入数\n");
+////			scanf("%d%d", &x, &y);
+////			printf("%d\n", arr[input](x, y));
+////		}
+////
+////		else
+////			printf(" 错误");
+////		
+////
+////
+////	} while (input);
+////}
+//int comp(const void* e1,const  void* e2) {
+//	if (*(float*)e1 == *(float*)e2)
+//		return 0;
+//	else if (*(float*)e1 > *(float*)e2)
+//		return 1;
+//	else
+//		return -1;
+//}
+//void test() {
+//	float f[5] = { 2.1,3.4,5.7,7.9,2.0 };
+//	int sz = sizeof(f) / sizeof(f[0]);
+//	qsort(f, sz, sizeof(f[0]), comp);
+//	int i = 0;
+//	for (i = 0; i < sz; i++) {
+//		printf("%f",f[i]);
+//	}
+//}
+//struct stu
+//{
+//	char name[10];
+//	int age;
+//
+//
+//};
+//int comp1(const void* e1, const void* e2) {
+//	return ((struct stu*)e1)->age - ((struct stu*)e2)->age;
+//}
+//int main() {
+//	test();
+//	struct stu s[3] = { {"zhangsan", 10}, { "lisi", 20 }, { "wangwu", 30 } };
+//	int sz = sizeof(s) / sizeof(s[0]);
+//	qsort(s, sz, sizeof(s[0]), comp1);
+//}
+void swap(char*buf1,char*buf2,int wide) {
+	int i = 0;
+	for (i = 0; i < wide; i++) {
+		char tmp = *buf1;
+		*buf1 = *buf2;
+		*buf2 = tmp;
+		buf1++;
+		buf2++;
+
+	}
+
+}
+int cmp1(void* e1, void* e2) {
+	return *(int*)e1 - *(int*)e2;
+}
+void bubble_sort(void* base, int sz, int wide, int(cmp1)(void* e1, void* e2)) {
+	int i = 0;
+	for (i = 0; i < sz - 1; i++) {
+		int j = 0;
+		for (j = 0; j < sz - 1 - i; j++) {
+			if (cmp1((char*)base + j * wide, (char*)base + (j + 1) * wide) > 0) {
+				swap((char*)base + j * wide, (char*)base + (j + 1) * wide,wide);
+			}
+		}
+	}
+}
+void test1() {
+	int arr[8] = { 9,8,7,6,5,4,32,1 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	bubble_sort(arr,sz,sizeof(arr[0]),cmp1);
+	int i = 0;
+	for (i = 0; i < sz ; i++) {
+		printf("%d", arr[i]);
+	}
+}
+#include<stdio.h>
+int main() {
+	test1();
+}
 //void test() {
 //	printf("***************************\n");
 //	printf("1.add   2.sub  3.mul  4.div\n");
